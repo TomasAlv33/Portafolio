e7d10333845d4e8a25211c39c7d3adb84e36652e
TomasAlv33 <tomas.agustin.alvarez@hotmail.com>
2021-10-30 12:02:43 -0300

    Implementacion de Js

---
+++ index.js
@@ -0,0 +1,36 @@
+const section1 = document.getElementById('presentacionContainer')
+const section2 = document.getElementById('sobreMiContainer')
+const section3 = document.getElementById('tecnologiasContainer')
+const section4 = document.querySelector('.proyecto')
+const section5 = document.querySelector('.p2')
+const section6 = document.querySelector('.p3')
+const section7 = document.querySelector('.p4')
+const section8 = document.getElementById('contactameContainer')
+
+const cargarImagen=(entradas , observador) =>{
+
+    entradas.forEach(entradas=> {
+        console.log(entradas)
+        if(entradas.isIntersecting){
+            entradas.target.classList.add('visible');
+        }
+    });
+
+}
+
+const observador = new IntersectionObserver(cargarImagen,{
+    root: null,
+    rootMargin:'200px' ,
+    threshold: 1.0
+})
+
+observador.observe(section1);
+observador.observe(section2);
+observador.observe(section3);
+observador.observe(section4);
+observador.observe(section5);
+observador.observe(section6);
+observador.observe(section7);
+observador.observe(section8);
+
+
